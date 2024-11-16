1722ac9f489d11c354fd2ecc7eb330725ee434dd
Wellersom Richardsom Voigt <wellersom.wrv@gmail.com>
2024-11-16 01:50:25 -0300

    define variáveis para conexão com banco de dados e importa valores do .env

--- src/db/connection.go
+++ src/db/connection.go
@@ -1,4 +1,17 @@
 package db
+
+import (
+	"os"
+)
+
+var (
+	host     = os.Getenv("host")
+	port     = os.Getenv("port")
+	user     = os.Getenv("user")
+	password = os.Getenv("pasword")
+	dbname   = os.Getenv("dbname")
+)
+
 var db *gorm.DB
 
 func Connection() *gorm.DB {

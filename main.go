744456a20e9ebc4c03ab3f9de980c4b9245b9b05
Wellersom Richardsom Voigt <wellersom.wrv@gmail.com>
2024-11-16 00:20:33 -0300

    define router

--- main.go
+++ main.go
@@ -1,3 +1,20 @@
 package main
+
+import (
+	"Financial-Transactions/src/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+var router = gin.Default()
+
 func main() {
+	getRoutes()
+	router.Run(":8081")
+
+}
+
+func getRoutes() {
+	v1 := router.Group("/v1")
+	routes.AddUserRoutes(v1)
 }
